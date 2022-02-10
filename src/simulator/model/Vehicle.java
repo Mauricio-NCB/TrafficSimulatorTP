e461746a832822efee1e63d97a84fdb61be1723c
Mauricio-NCB <mncalderon2001@gmail.com>
2022-02-10 20:37:13 +0100

    Vehicle a medias

---
+++ src/simulator/model/Vehicle.java
@@ -0,0 +1,51 @@
+package simulator.model;
+
+import java.util.List;
+
+import org.json.JSONObject;
+
+public class Vehicle extends SimulatedObject {
+
+	private int location;
+	private int speed;
+	private int maxSpeed;
+	private int contClass;
+	private List<Junction> itinerary;
+	private VehicleStatus status;
+	private int totalCO2;
+	private Road road;
+	
+	Vehicle(String id, int maxSpeed, int contClass, List<Junction> itinerary) {
+		super(id);
+		try {
+			if (maxSpeed < 0 || contClass < 0 || contClass > 10 || itinerary.size() < 2) {
+				throw new Exception("Datos del vehiculo son invalidos");
+			}
+			this.maxSpeed = maxSpeed;
+			this.contClass = contClass;
+			this.itinerary = itinerary;
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	@Override
+	void advance(int time) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public JSONObject report() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getLocation() {
+		return location;
+	}
+	
+	public 
+	
+}
