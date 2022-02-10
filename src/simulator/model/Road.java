ecddcbe739b6a93c7bb8b33c926b8150d92faa0d
margal15 <margal15@ucm.es>
2022-02-10 21:20:32 +0100

    Creado el Road

--- src/simulator/model/Road.java
+++ src/simulator/model/Road.java
@@ -1,5 +1,108 @@
 package simulator.model;
 
-public class Road {
+import java.util.List;
 
+import org.json.JSONObject;
+
+public abstract class Road extends SimulatedObject{
+	
+	private int length;
+	private Junction srcJunc;
+	private Junction destJunc;
+	private int maxSpeed;
+	private int currentSpeedLimit;
+	private int contLimit;
+	private Weather weather;
+	private int totalCont;
+	private List<Vehicle> vehicles;
+	
+	Road(String id, Junction srcJunc, Junction destJunc, int maxSpeed, int contLimit, int length, Weather weather){
+		super(id);
+		try{
+			
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	@Override
+	void advance(int time) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public JSONObject report() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public int getLength(){
+		return length;
+	}
+	
+	public int getContLimit(){
+		return contLimit;
+	}
+	
+	public int getMaxSpeed(){
+		return maxSpeed;
+	}
+	
+	public int getTotalCO2(){
+		return totalCont;
+	}
+	
+	public int getSpeedLimit(){
+		return currentSpeedLimit;
+	}
+	
+	public Junction getDest(){
+		return destJunc;
+	}
+	
+	public Junction getSrc(){
+		return srcJunc;
+	}
+	
+	public Weather getWeather(){
+		return weather;
+	}
+	
+	public List<Vehicle> getVehicles(){
+		return vehicles;
+	}
+
+	public void setWeather(Weather weather) {
+		try{
+			if(weather==null){
+				throw new Exception("Clima igual a nulo");
+			}
+			this.weather = weather;
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public void addContamination(int c){
+		try{
+			if(c<0){
+				throw new Exception("Contaminacion negativa");
+			}
+			this.totalCont += c;
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	public abstract void reduceTotalContamination();
+	
+	public abstract void updateSpeedLimit();
+	
+	public abstract int calculateVehicleSpeed(Vehicle v);
+	
+	
+	
 }
