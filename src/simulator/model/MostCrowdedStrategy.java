6260546344719506b9410bf26ba605c5ae2ed720
Mauricio-NCB <mncalderon2001@gmail.com>
2022-03-05 14:12:03 +0100

    MostCrowdedStrategy corregido

--- src/simulator/model/MostCrowdedStrategy.java
+++ src/simulator/model/MostCrowdedStrategy.java
@@ -15,35 +15,43 @@ public class MostCrowdedStrategy implements LightSwitchingStrategy {
 			int lastSwitchingTime, int currTime) {
 		// TODO Auto-generated method stub
 		
-		if (roads.size() == 0) {
+		if (roads.isEmpty()) {
 			return -1;
 		}
+		
 		if (currGreen == -1) {
 			
 			int indexMaxQ = 0;
+			int lengthQ = 0;
 			
 			for (int i = 0; i < qs.size(); i++) {
-				if (qs.get(i).size() > indexMaxQ) {
-					indexMaxQ = qs.get(i).size();
+				
+				if (qs.get(i).size() > lengthQ) {
+					lengthQ = qs.get(i).size();
+					indexMaxQ = i;
 				}
 			}
 			
 			return indexMaxQ;
 		}
+		
 		if (currTime - lastSwitchingTime < timeSlot) {
 			return currGreen;
 		}
 		
 		int indexMaxQ = ((currGreen + 1) % qs.size());
+		int lengthQ = 0;
 		
 		for (int i = ((currGreen + 1) % qs.size()); i < qs.size(); i++) {
-			if (qs.get(i).size() > indexMaxQ) {
-				indexMaxQ = qs.get(i).size();
+			if (qs.get(i).size() > lengthQ) {
+				lengthQ = qs.get(i).size();
+				indexMaxQ = i;
 			}
 		}
 		for (int i = 0; i < ((currGreen + 1) % qs.size()); i++) {
-			if (qs.get(i).size() > indexMaxQ) {
-				indexMaxQ = qs.get(i).size();
+			if (qs.get(i).size() > lengthQ) {
+				lengthQ = qs.get(i).size();
+				indexMaxQ = i;
 			}
 		}
 		
